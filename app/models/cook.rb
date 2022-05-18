acef78a8fec5c994b00bcf8c02d62eea64a2ab20
umeharaharuki <jasper1116lopez@icloud.com>
2022-05-18 17:54:18 +0900

    バリデーション記述

--- app/models/cook.rb
+++ app/models/cook.rb
@@ -1,2 +1,16 @@
 class Cook < ApplicationRecord
+  belongs_to :user
+  with_options presence: true do
+    validates :cook_name
+    validates :recipe
+    validates :material
+    validates :country_id
+    validates :serving_id
+    validates :image
+  end
+
+  with_options numericality: { other_than: 1, message: 'is valid' } do
+    validates :country_id
+    validates :serving_id
+  end
 end
