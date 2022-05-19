e5447e86b42bf67bf8aa5fa0049a9cc24066ae4d
umeharaharuki <jasper1116lopez@icloud.com>
2022-05-19 12:46:49 +0900

    保存完了

--- app/models/cook.rb
+++ app/models/cook.rb
@@ -1,6 +1,11 @@
 class Cook < ApplicationRecord
-  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :country
+  belongs_to :serving
+
   has_one_attached :image
+  belongs_to :user
+
   with_options presence: true do
     validates :cook_name
     validates :recipe
@@ -10,7 +15,7 @@ class Cook < ApplicationRecord
     validates :image
   end
 
-  with_options numericality: { other_than: 1, message: 'is valid' } do
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
     validates :country_id
     validates :serving_id
   end
